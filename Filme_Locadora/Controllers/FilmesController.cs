fa8c1bb2cd1324c7ad239d8efd5c59f5702cadb4
Gisele da Silva Cidral <63521270+Gisele7@users.noreply.github.com>
2022-05-01 15:52:41 -0300

    Add project files.

---
+++ Filme_Locadora/Controllers/FilmesController.cs
@@ -0,0 +1,79 @@
+﻿using Filme_Locadora.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Web;
+using Filme_Locadora.Models;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace Filme_Locadora.Controllers
+{
+    public class FilmesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Incluir()
+        {
+            //case sensitive. pegamos da classe categorias o tem Id e Descricao
+
+            //Vamos encaminhar p view a lista de categorias pra que quando categoria for selecionada, o id faça parte do produto.
+            ViewBag.ListaCategorias = new SelectList(DbFilmes.ListarCategorias(), "Id", "Descricao");
+            return View();
+        }
+        //[HttpPost]
+        //// POSTED DA IMAGEM 
+        //public ActionResult Incluir(Tbfilmes filme, HttpPostedFileBase image)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return Incluir();
+        //    }
+        //    // IMAGEM 
+        //    if (image != null)
+        //    {
+        //        filme.MimeType = image.ContentType;
+        //        filme.Foto = new byte[image.ContentLength];
+
+        //        //Copiando os bytes da imagem recebida como parâmetro, na propriedade foto.
+        //        //Parâmetros: 1° Quem vai receber os bytes? | 2° A partir de posição que a gente vai começar a tratar esses bytes? | 3° Tratar até o final 
+        //        image.InputStream.Read(filme.Foto, 0, image.ContentLength);
+        //    }
+
+        //    DbFilmes.Incluir(filme);
+        //    return RedirectToAction("Listar");
+        //}
+
+        ////IMAGEM 
+        //public FileResult BuscarProduto(int id)
+        //{
+        //    try
+        //    {
+        //        var produto = DbFilmes.Buscar(id);
+        //        if (produto != null)
+        //        {
+        //            if (produto.Foto != null)
+        //            {
+        //                return File(produto.Foto, produto.MimeType);
+        //            }
+        //        }
+        //        return File("~/Images/ND.png", "image/jpeg");
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        throw;
+        //    }
+
+
+        public ActionResult Listar()
+        {
+            return View(DbFilmes.Listar());
+        }
+    }
+
+}
---
+++ Filme_Locadora/DataAccess/DbFilmes.cs
@@ -0,0 +1,38 @@
+﻿namespace Filme_Locadora.DataAccess
+{
+    public static class DbFilmes
+    {
+        public static void Incluir(Tbfilmes filme)
+        {
+            using (var ctx = new VIDEO_LOCADORAContext())
+            {
+                ctx.Tbfilmes.Add(filme);
+                ctx.SaveChanges();
+            }
+        }
+        public static Tbfilmes Buscar(int id)
+        {
+            using (var ctx = new VIDEO_LOCADORAContext())
+            {
+                return ctx.Tbfilmes.FirstOrDefault(p => p.Id == id);
+            }
+        }
+        public static IEnumerable<Tbcategorias> ListarCategorias()
+        {
+            using (var ctx = new VIDEO_LOCADORAContext())
+            {
+                return ctx.Tbcategorias.ToList();
+            }
+        }
+
+        public static IEnumerable<Tbfilmes> Listar()
+        {
+            using (var ctx = new VIDEO_LOCADORAContext())
+            {
+                return ctx.Tbfilmes.ToList();
+            }
+        }
+    }
+
+
+}
